2819f2a90bec8bf6ec009da2b9273eac99dda3d2
Josep <jcanalsb98@gmail.com>
2023-06-03 11:12:29 +0200

    add guards rutas

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { PublicGuard } from './guards/public.guard';
 
 
 // localhost:/4200/auth/
@@ -11,9 +12,18 @@ const routes: Routes = [
     path:'',
     component: LayoutPageComponent,
     children: [
-      { path: 'login', component: LoginPageComponent },
-      { path: 'new-account', component: RegisterPageComponent },
-      { path: '**', redirectTo: 'login', },
+      {
+        path: 'login',
+        component: LoginPageComponent
+      },
+      {
+        path: 'new-account',
+        component: RegisterPageComponent
+       },
+      {
+        path: '**',
+        redirectTo: 'login'
+       },
     ]
   }
 ];
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -8,9 +11,24 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
   public sidebarItems = [
     {label: 'Listado', icon: 'label', url: './list' },
     {label: 'Añadir', icon: 'add', url: './new-hero' },
     {label: 'Buscar', icon: 'search', url: './search' },
   ]
+
+  get user():User | undefined{
+    return this.authService.currentUser;
+  }
+
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
+
 }
